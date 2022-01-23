a0848fedf763189ed9e8254d8cbfca64dbbeb385
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-23 19:23:39 +0600

    Logout functionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -68,6 +68,23 @@ exports.login = async (req, res) => {
   }
 };
 
+exports.logout = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", null, { expires: new Date(Date.now()), httpOnly: true })
+      .json({
+        success: true,
+        message: "logged out",
+      });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 exports.followUser = async (req, res) => {
   try {
     const userToFollow = await User.findById(req.params.id);

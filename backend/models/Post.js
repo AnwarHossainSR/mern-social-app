80ff56e28a2493ba91aa1c542e1cdd7543c03221
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-23 11:29:31 +0600

    create post and user model with schema

---
+++ backend/models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { stringify } = require("nodemon/lib/utils");
+
+const postSchecma = new mongoose.Schema({
+  caption: String,
+  imageUrl: {
+    public_id: String,
+    url: String,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: { type: mongoose.Types.ObjectId, ref: "User" },
+      comment: { type: mongoose.Schema.Types.ObjectId, required: true },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Post", postSchecma);

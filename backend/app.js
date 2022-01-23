1e5c5a72edc02ec6d59aeb047a89088421bff915
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-23 11:48:09 +0600

    create controller and add with routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const { createPost } = require("./controllers/Post");
 const app = express();
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
+
+//using middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//importing Routes
+const post = require("./routes/Post");
+//using Route
+app.use("/api/v1", post);
 module.exports = app;

505d06070e19adaeee82cb4d3d86ac68a37e4f52
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-25 22:23:51 +0600

    delete comment

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -6,6 +6,7 @@ const {
   getPostsOfFollowing,
   updateCaption,
   commentOnPost,
+  deleteComment,
 } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
@@ -14,7 +15,10 @@ const router = express.Router();
 router.route("/posts").get(isAuthenticated, getPostsOfFollowing);
 router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/comment/:id").put(isAuthenticated, commentOnPost);
+router
+  .route("/post/comment/:id")
+  .put(isAuthenticated, commentOnPost)
+  .delete(isAuthenticated, deleteComment);
 router
   .route("/post/:id")
   .get(isAuthenticated, likeAndUnlikePost)
